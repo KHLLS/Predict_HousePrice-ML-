086c60a0ee5ce68a927a1f8e7e2871719a66770c
Kahlil <Kahlil122333@gmail.com>
2026-08-06 20:25:54 +0700

    fix path

--- app/utils/location.py
+++ app/utils/location.py
@@ -1,8 +1,8 @@
-from app.ml.inference import predictor
+# from app.ml.inference import predictor
 
 def load_city():
-    if predictor.pipeline is None:
-        return ["Jakarta Pusat", "Jakarta Selatan", "Jakarta Timur", "Jakarta Barat", "Jakarta Utara"]
-    feature = list(predictor.pipeline.feature_names_in_)
-    city_map = [c.removeprefix("city_") for c in feature if c.startswith("city_")]
-    return city_map
+    # if predictor.pipeline is None:
+    return ["Jakarta Pusat", "Jakarta Selatan", "Jakarta Timur", "Jakarta Barat", "Jakarta Utara"]
+    # feature = list(predictor.pipeline.feature_names_in_)
+    # city_map = [c.removeprefix("city_") for c in feature if c.startswith("city_")]
+    # return city_map
